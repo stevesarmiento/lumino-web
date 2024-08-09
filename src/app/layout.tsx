b2b9e439fafcc9c5d78e382a329c37ef93b0941d
steven <sarmiento.steven@gmail.com>
2024-08-09 18:47:29 -0500

    site update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <PlausibleProvider domain="luminoapp.com" />
+        <PlausibleProvider domain="luminoapp.xyz" />
       </head>
         <body className={inter.className}>{children}</body>
     </html>
